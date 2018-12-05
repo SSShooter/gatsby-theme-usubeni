c216cfab1335643fc0f1facb7fbfd971d2b35b05
ssshooter <472356884@qq.com>
2018-12-05 23:43:22 +0800

    :bug: use Helmet inject js code instead of adding inline

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -19,7 +19,18 @@ class BlogPostTemplate extends React.Component {
           htmlAttributes={{ lang: 'en' }}
           meta={[{ name: 'description', content: siteDescription }]}
           title={`${post.frontmatter.title} | ${siteTitle}`}
-        />
+        >
+          <script>
+            {`(function(d) {
+                var config = {
+                  kitId: 'xrb8rdl',
+                  scriptTimeout: 3000,
+                  async: true
+                },
+                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+              })(document);`}
+          </script>
+        </Helmet>
         <h1 className="css-title">{post.frontmatter.title}</h1>
         <p
           className="css-date"
