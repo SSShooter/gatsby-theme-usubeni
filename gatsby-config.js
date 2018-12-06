74f52a2c3102c4e59058ff41f05b07d16b523245
ssshooter <472356884@qq.com>
2018-12-06 22:16:50 +0800

    :sparkles: add google trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-130598883-1`,
       },
     },
     `gatsby-plugin-feed`,
