59b7c43381da3154915d702e45192dd8a09b5a4f
ssshooter <472356884@qq.com>
2019-09-05 21:09:12 +0800

    樱花特效更新，未做老浏览器兼容

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,38 @@ import 'prismjs/themes/prism.css'
 
 import '../css/global.scss'
 
+const randomGen = (min, max) => {
+  return Math.random() * (max - min) + min
+}
+const keyframesGen = () => [
+  {
+    transform: 'translateY(0) translateX(0) translateZ(0) rotate3d(0, 0, 0, 0)',
+  },
+  {
+    transform: `translateY(${randomGen(
+      0,
+      400
+    )}px) translateX(2560px) translateZ(${randomGen(
+      0,
+      200
+    )}px) rotate3d(${randomGen(0, 1)}, ${randomGen(0, 1)}, ${randomGen(
+      0,
+      1
+    )}, ${randomGen(-200, 200)}deg)`,
+  },
+]
+
+const optionGen = () => ({
+  iterations: Infinity,
+  delay: randomGen(-4000, 4000),
+  duration: randomGen(14000, 22000),
+})
+
 class Layout extends React.Component {
   state = {
     menuState: false, // false for close, true for open
     keyword: '',
+    animation: [],
   }
   toggleMenuState = () => {
     this.setState({
@@ -26,6 +54,29 @@ class Layout extends React.Component {
       'https://www.google.co.jp/search?q=site%3Assshooter.com+' +
         this.state.keyword
     )
+  }
+  componentDidMount() {
+    let petal = document.querySelectorAll('.petal')
+    for (let i = 0; i < petal.length; i++) {
+      this.state.animation[i] = petal[i].animate(keyframesGen(), optionGen())
+    }
+    let a = document.querySelectorAll('a')
+
+    for (let i = 0; i < a.length; i++) {
+      a[i].onmouseenter = this.slowMotion
+      a[i].onmouseleave = this.backNormal
+      // 移动端用 touchstart touchend
+    }
+  }
+  slowMotion = () => {
+    for (let i = 0; i < this.state.animation.length; i++) {
+      this.state.animation[i].playbackRate = 0.4
+    }
+  }
+  backNormal = () => {
+    for (let i = 0; i < this.state.animation.length; i++) {
+      this.state.animation[i].playbackRate = 1
+    }
   }
   render() {
     const { menuState } = this.state
