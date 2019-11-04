aae1578626b27f19f5f737dc3ccbc97941bcb74f
zhoudejie <zhoudj94@gmail.com>
2019-11-04 15:05:46 +0800

    写错地址

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
-export let apiUrl = 'https://comment-sys.herokuapp.com/api/comment'
+export let apiUrl = 'https://comment-sys.herokuapp.com'
 
 // https://comment-sys.herokuapp.com/api/comment
 //  'http://49.232.162.204:7001'
