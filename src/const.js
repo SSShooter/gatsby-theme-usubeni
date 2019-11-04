d71022ea8583bdfab26191374b13b5956ce318d3
zhoudejie <zhoudj94@gmail.com>
2019-11-04 15:05:18 +0800

    迫于https回退接口

--- src/const.js
+++ src/const.js
@@ -1,3 +1,4 @@
-export let apiUrl = 'http://49.232.162.204:7001'
+export let apiUrl = 'https://comment-sys.herokuapp.com/api/comment'
 
 // https://comment-sys.herokuapp.com/api/comment
+//  'http://49.232.162.204:7001'
