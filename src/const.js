214a6662d35fb00acca56712d1216b8610f17269
zhoudejie <zhoudj94@gmail.com>
2019-11-05 12:27:50 +0800

    修改apiurl

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
-export let apiUrl = 'https://api.pocky.monster'
+export let apiUrl = 'https://api.pocky.monster:8001'
 
 // https://comment-sys.herokuapp.com/api/comment
 //  'http://49.232.162.204:7001'
