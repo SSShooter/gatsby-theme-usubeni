78b0da5f044d4dd71d729d7f914decdd20433962
zhoudejie <zhoudj94@gmail.com>
2019-11-05 12:32:55 +0800

    草，apiurl修改

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
-export let apiUrl = 'https://api.pocky.monster:8001'
+export let apiUrl = 'https://pocky.monster:8001'
 
 // https://comment-sys.herokuapp.com/api/comment
 //  'http://49.232.162.204:7001'
