ab404e35c123ec1399b57c4a53c781fc6e2bb2d3
ssshooter <zhoudj94@gmail.com>
2021-04-13 13:28:41 +0800

    字体来源从@import改为link标签

--- src/html.js
+++ src/html.js
@@ -21,7 +21,8 @@ export default class HTML extends React.Component {
             content="email=no,telephone=no,address=no"
           />
           <meta name="baidu-site-verification" content="rVEuQMP8C6" />
-          {/* <link href="https://fonts.googleapis.com/css?family=Noto+Serif+SC:300&subset=chinese-simplified" rel="stylesheet"></link> */}
+          <link rel="preconnect" href="https://fonts.gstatic.com">
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap" rel="stylesheet"></link>
           <link
             rel="apple-touch-icon"
             sizes="180x180"
